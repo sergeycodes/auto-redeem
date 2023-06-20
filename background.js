5c34ff81fd1f103bc20d2b56b49839d8ae60385a
Sergey Barzul <sbarzul@uw.edu>
2023-06-19 19:38:33 -0700

    removed activetab

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({text: 'OFF'});
-});
+
 
 const extensions = 'https://developer.chrome.com/docs/extensions'
 const webstore = 'https://developer.chrome.com/docs/webstore'
