5e86f48a862eb866ded8b52aa1e75d1636399b1d
Sergey Barzul <sbarzul@uw.edu>
2023-06-19 20:30:55 -0700

    testing auto-redeem

--- scripts/auto-redeem.js
+++ scripts/auto-redeem.js
@@ -2,16 +2,7 @@
  if(claimButton){
   claimButton.click();
   console.log("Points claimed!");
-
  }
-  function claimPoints() {
-  
-  if (claimButton !== null) {
-    claimButton.click();
-    console.log("Points claimed!");
-  }
-}
 
-setInterval(claimPoints, 60000); // It will check every 1 minute
   
 
