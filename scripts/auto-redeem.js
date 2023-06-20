9612e634a88cfda124917fbd2b056a7ae2c256c4
Sergey Barzul <sbarzul@uw.edu>
2023-06-19 22:52:47 -0700

    worked on getting streamer name

--- scripts/auto-redeem.js
+++ scripts/auto-redeem.js
@@ -1,11 +1,18 @@
 
-const claimButton = document.querySelector(".claimable-bonus__icon");
 
-if(document.querySelector(".claimable-bonus__icon")){
-  console.log("Points claimed!");
-  claimButton.click();
-}else{
-  console.log("No points to claim.");
+setInterval(function(){ 
+  const claimButton = document.querySelector(".claimable-bonus__icon");
+
+  if(claimButton !== null){
+    console.log("Points claimed!");
+    claimButton.click();
+  }else{
+    console.log("No points to claim.");
+  }
+}, 1000); // every 10 seconds
+
+
+const streamerName = document.getElementsByClassName("tw-title").innerText;
+if(streamerName !== undefined){
+  console.log("Streamer name: " + streamerName);
 }
-
-
