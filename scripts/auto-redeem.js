8ee3cdab648b5c403dcb7b7422a2ab6350e2a7b1
Sergey Barzul <sbarzul@uw.edu>
2023-06-19 22:10:53 -0700

    fixed display

--- scripts/auto-redeem.js
+++ scripts/auto-redeem.js
@@ -1,6 +1,11 @@
 
-  console.log("Points claimed!");
- 
+const claimButton = document.querySelector(".claimable-bonus__icon");
 
-  
+if(document.querySelector(".claimable-bonus__icon")){
+  console.log("Points claimed!");
+  claimButton.click();
+}else{
+  console.log("No points to claim.");
+}
+
 
